a04c5f09a70ff66bbe247e2e597ac718aeecefcf
SolalDR <solal.dussout-revel@hotmail.fr>
2020-10-20 17:45:58 +0200

    fix(ormconfig): Improve ORMConfig to handle heroku

--- ormconfig.js
+++ ormconfig.js
@@ -1,12 +1,24 @@
 const AdminUser = require('nestjs-admin').AdminUserEntity
 
+const databaseURL = process.env.DATABASE_URL
+
+if (databaseURL) {
+  const postgresUrl = /^(.+?):\/\/(.+?):(.+?)@([\w-.]+?):(\d{2,4})\/(.+?)$/
+  const matches = databaseURL.match(postgresUrl)
+  if (matches) {
+    var [_, DB_PROVIDER, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME] = matches;
+  } else {
+    throw new Error(`config error - DATABSE_URL given but with an invalid format`);
+  }
+}
+
 module.exports = {
   "type": "postgres",
-  "host": "localhost",
-  "port": 5432,
-  "username": "user",
-  "password": "example",
-  "database": "database",
+  "host": DB_HOST || process.env.DB_HOST,
+  "port": DB_PORT || process.env.DB_PORT,
+  "username": DB_USER || process.env.DB_USER,
+  "password": DB_PASSWORD || process.env.DB_PASSWORD,
+  "database": DB_NAME || process.env.DB_NAME,
   "entities": [
     "src/**/*.entity.ts", AdminUser
   ],
